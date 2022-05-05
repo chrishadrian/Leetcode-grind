9d215e533ad328f4ef24cfac2898b6129c112e5e
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-05-05 01:32:54 -0700

    Update 4FB.py

--- 4FB.py
+++ 4FB.py
@@ -13,3 +13,9 @@ class Solution:
                 result.append(str(i))
         
         return result
+
+## Much faster algorithm
+# return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+
+## Reflection:
+# Nothing new here...
