cbdd4dcd40f2f168f68e132769e0ce8a6d1235e9
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-05-07 16:57:19 -0700

    Problem 2

---
+++ 7NSRNZ.py
@@ -0,0 +1,18 @@
+class Solution:            
+    def numberOfSteps(self, num: int) -> int:
+        def countSteps(num: int, steps: int) -> int:
+            # base case
+            if (not num):
+                return steps
+
+
+            if (num % 2 == 0):
+                return countSteps(num//2, 1+steps   )
+            else:
+                return countSteps(num-1, steps+=1)
+                
+        return countSteps(num, 0)
+        
+# Runtime: 30 ms, faster than 89.31% of Python3 online submissions for Number of Steps to Reduce a Number to Zero.
+# Memory Usage: 13.8 MB, less than 53.76% of Python3 online submissions for Number of Steps to Reduce a Number to Zero.
+
