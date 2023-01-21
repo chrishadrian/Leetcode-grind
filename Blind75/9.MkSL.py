50a3fa66a55fa7876080923b304b585e6c7cadc3
Christopher Wong <chrishadrianwong@gmail.com>
2023-01-20 23:29:30 -0800

    LCG-2: Updated leet code list

--- Blind75/9.MkSL.py
+++ Blind75/9.MkSL.py
@@ -15,7 +15,7 @@ class Solution:
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i+1] if (i+1) < len(lists) else None
-                mergedList.append(self.mergeTwoLists(l1, l2))
+                mergedList.append(self.mergeTwoLinkedLists(l1, l2))
             
             lists = mergedList
         return lists[0]
